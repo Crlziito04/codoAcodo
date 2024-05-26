62455f083c032c03525f0fb8eb1f73cd8eab2564
Carlos Molina <105681853+Crlziito04@users.noreply.github.com>
2024-05-25 23:05:20 -0300

    prueba2

--- script/fetch.js
+++ script/fetch.js
@@ -1,4 +1,22 @@
-fetch("https://github.com/Crlziito04/codoAcodo/script/data.json")
+const renderCardsPresentacion = (data) => {
+  const containerPresentacion = document.getElementById("containerPresentacion");
+
+  data.forEach(item => {
+    const article = document.createElement('article');
+    article.innerHTML = `<img
+            src=${item.img}
+            alt=${item.nombre}
+          />
+          <a href="#${item.nombre}" class="button">
+                        <h2>${item.nombre}</h2>
+                    </a>
+                    <p>${item.description}</p>
+          `
+    containerPresentacion.appendChild(article);
+  })
+}
+
+fetch("https://crlziito04.github.io/codoAcodo/data.json")
   .then(response => {
     if (!response.ok) {
       throw new Error("Network response was not ok " + response.statusText);
@@ -6,7 +24,6 @@ fetch("https://github.com/Crlziito04/codoAcodo/script/data.json")
     return response.json();
   })
   .then(data => {
-    // Procesamiento de la info que llega de la API
-    console.log(data, "********");
+    renderCardsPresentacion(data);
   })
   .catch(error => console.log("Ocurrió un error! " + error));
